3ee513a1b68a2bf9f84599ac61114364ddd6a98b
rish attiq <arnishattiq@gmail.com>
2024-07-12 17:23:34 +0500

    post,delete,update

--- src/app/components/post-api/post-api.component.ts
+++ src/app/components/post-api/post-api.component.ts
@@ -1,55 +1,81 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, inject ,OnInit} from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-api',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './post-api.component.html',
-  styleUrl: './post-api.component.css'
+  styleUrls: ['./post-api.component.css']
 })
 export class PostApiComponent implements OnInit {
+  user: any = {
+    id: '',
+    title: '',
+    body: ''
+  };
 
+  users: any[] = [];
 
-  
-user: any={
-  
-    "id": "",
-    "title": "",
-    "body": ""
-};
+  http = inject(HttpClient);
 
-http = inject(HttpClient);
-
-ngOnInit(): void{
-
-
-}
-
-onSubmit(){
-  this.http.post('https://jsonplaceholder.typicode.com/posts', this.user).subscribe((data: any)=>{
-   if(data){
-
-    alert("user added successfully");
-    // this.getusers();
-    console.log(data);
-   }
-   else{
-    alert(data.message);
-   }
-    })
+  ngOnInit(): void {
+    this.getusers();
   }
 
-// getusers(){
-//   this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((data: any)=>{
+  onEdit(data:any){
+    this.user=data;
+  }
 
-//     this.user=data;
-//   })
-// }
+  
 
+  onDelete(id:number){
 
+    const isDelete=confirm("are u sure to delete?");
+
+    if(isDelete){
+      this.http.delete(`https://jsonplaceholder.typicode.com/posts/${id}`).subscribe((data:any) => {
+        if(data){
+          alert('User deleted successfully');
+          this.getusers();
+        }
+        else{
+          alert(data.message)
+        }
+       
+      });
+    }
+   
+    
+  }
+
+  onUpdate(){
+    this.http.post('https://jsonplaceholder.typicode.com/posts', this.user).subscribe((data: any) => {
+      if (data) {
+        alert('User updated successfully');
+     this.getusers();
+      } else {
+        alert(data.message);
+      }
+    });
+  }
+
+  onSubmit() {
+    this.http.post('https://jsonplaceholder.typicode.com/posts', this.user).subscribe((data: any) => {
+      if (data) {
+        alert('User added successfully');
+        this.getusers();
+      } else {
+        alert(data.message);
+      }
+    });
+  }
+
+  getusers() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe((data: any) => {
+      this.users = data;
+    });
+  }
 }
-
-
